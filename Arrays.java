b6daab46860e570b8d4d8fde830ce94b3fd00602
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2023-03-03 17:19:32 +0300

    arrays

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+
+public class Arrays {
+  
+   public static void main(String[] args)
+ {
+     int a [] = new int[5];
+     a[3] = 8; // 0,0,0,8,0
+     System.out.println(a[3]);
+     int b []={1,3,5,7,9};
+     
+     String str[] = {"purple","pink","yellow"};
+     System.out.println(str[2]+b[1]+str[0]);
+     
+     
+
+}
+  
+}
