3dd05dab21742a05a7bd48c68205d304f4efc0ef
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2023-03-03 17:18:01 +0300

    operators

---
+++ Operators.java
@@ -0,0 +1,17 @@
+
+public class Operators {
+  public static void main(String[] args){
+    int a=10,b=20,c=30;
+    System.out.println("result1:"+(a+b));
+    System.out.println("result2:"+ a+b);
+    System.out.println("result3:"+ a*b);
+    System.out.println("result4:"+(float) (a/b));
+    System.out.println("result5:"+(a-b));
+    System.out.println("result5:"+(10>>2));//1010 0101 0010
+    System.out.println("result6:"+(a<b));
+    System.out.println("result6:"+(a>b));
+    System.out.println("result6:"+(a==b));
+    
+  }
+  
+}
